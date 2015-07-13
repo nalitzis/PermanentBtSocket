7e2017a32f2ada88634af96ceb6850f2b9ffe321
Adolfo Bulfoni <Adolfo.Bulfoni@tomtom.com>
2015-07-13 11:42:28 +0200

    made prj compatible with our sdk

--- app/src/main/java/ado/permanentbtsocket/BtConstants.java
+++ app/src/main/java/ado/permanentbtsocket/BtConstants.java
@@ -7,6 +7,11 @@ import java.util.UUID;
  */
 public class BtConstants {
 
-    public static final String REMOTE_SERVER_BT_ADDRESS = "CC:FA:00:36:3F:51";
+    //ado nexus 5
+    //public static final String REMOTE_SERVER_BT_ADDRESS = "CC:FA:00:36:3F:51";
+
+    //s3 ams office
+    public static final String REMOTE_SERVER_BT_ADDRESS = "98:52:B1:4C:28:E1";
+
     public static final UUID REMOTE_SERVER_UUID = UUID.fromString("cf5169a6-2897-11e5-b345-feff819cdc9f");
 }
